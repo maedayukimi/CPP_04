dcd2176b19592766c535fb01a7f1a6b34aa11700
robotsamurai <maedayukimi@icloud.com>
2025-12-24 17:18:36 +0900

    test added

--- ex03/main.cpp
+++ ex03/main.cpp
@@ -4,6 +4,7 @@
 #include "Cure.hpp"
 
 int	main()
+/*
 {
 	IMateriaSource* src = new MateriaSource();
 	src->learnMateria(new Ice());
@@ -26,3 +27,70 @@ int	main()
 
 	return 0;
 }
+*/
+/*
+{
+	MateriaSource src;
+
+	src.learnMateria(new Ice());
+	src.learnMateria(new Cure());
+	src.learnMateria(new Ice());
+	src.learnMateria(new Cure());
+	src.learnMateria(new Ice());
+
+	AMateria* m = src.createMateria("ice");
+	if (m)
+		std::cout << "Created ice\n";
+	else
+		std::cout << "Failed\n";
+
+	delete m;
+}
+*/
+/*
+{
+	MateriaSource src;
+	src.learnMateria(new Ice());
+
+	AMateria* m = src.createMateria("fire");
+	if (!m)
+		std::cout << "NULL OK\n";
+}
+*/
+/*
+{
+	Character a("A");
+	a.equip(new Ice());
+	a.equip(new Cure());
+
+	Character b(a);
+
+	a.use(0, a);
+	b.use(0, b);
+}
+*/
+/*
+{
+	Character a("A");
+	a.equip(new Ice());
+
+	Character b("B");
+	b = a;
+
+	a.use(0, a);
+	b.use(0, b);
+}
+*/
+/*
+{
+	Character me("me");
+	AMateria* ice = new Ice();
+
+	me.equip(ice);
+	me.unequip(0);
+
+	ice->use(me);
+
+	delete ice;
+}
+*/
